23066a30d664549ee5239d86f450394a076c775d
aterdik <a.terdik93@gmail.com>
2022-05-09 16:33:04 +0200

    Initial

---
+++ input_demo.py
@@ -0,0 +1,12 @@
+name = input("Mi a neved?\n")
+print("Hello " + name + "!")
+
+# Mi a kedvenc számod?
+# 6
+# favourite_number
+# A kedvenc számod: 6
+#A kedvenc számod duplája: 12
+favourite_number = input("Mi a kedvenc számod?\n")
+print("A kedvenc számod: " + favourite_number)
+
+print("A kedvenc számod duplája: " + favourite_number * 2)
